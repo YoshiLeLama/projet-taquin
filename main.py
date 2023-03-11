9d7043b004d08d31289cf5658c30c090e5f8ba94
Antoine Roumilhac <antoine.roumilhac@outlook.fr>
2023-03-12 00:21:51 +0100

    ENFIN CA MARCHE

--- main.py
+++ main.py
@@ -15,64 +15,62 @@ POIDS_TUILES = [
     1, 1, 1, 1, 1, 1, 1, 1,
 ]
 
-K = 1
+K = 6
 
 # COEFF_NORMAL = [4, 1, 4, 1, 4, 1]
 
 Etat = namedtuple('Etat', ['parent', 'liste_deplacement',
-                           'heuristique', 'profondeur'])
+                           'cout'])
 Etat.__annotations__ = {
-    'parent': Etat, 'liste_deplacement': list[str], 'heuristique': int, 'profondeur': int}
+    'parent': Etat, 'liste_deplacement': list[str], 'cout': int}
+
 
 def expanse(plateau_initial: list[int], etat_choisi: Etat):
     # régle et mouvement à définir.
     result: list[Etat] = []
-
-    grille = deplacement(etat_choisi.liste_deplacement, plateau_initial)
 
     for d in ['N', 'S', 'O', 'E']:
         nouveaux_deplacements = etat_choisi.liste_deplacement[:]
         nouveaux_deplacements.append(d)
         result.append(Etat(parent=etat_choisi,
                            liste_deplacement=nouveaux_deplacements,
-                           heuristique=heuristique(K, deplacement(d, grille)),
-                           profondeur=etat_choisi.profondeur+1))
-
+                           cout=len(nouveaux_deplacements) + heuristique(K, deplacement(nouveaux_deplacements, plateau_initial))))
     return result
+
 
 def inserer_etat(file_etat: list[Etat], etat: Etat):
     for i in range(0, len(file_etat)):
-        if file_etat[i].heuristique + file_etat[i].profondeur > etat.heuristique + etat.profondeur:
+        if file_etat[i].cout > etat.cout:
             file_etat.insert(i, etat)
             return
-        
+
     file_etat.append(etat)
+
 
 def astar(plateau_initial):
     # n est la taille du taquin
-    frontiere = [Etat(parent=None, liste_deplacement=[], heuristique=heuristique(K, plateau_initial), profondeur=0)]
+    frontiere = [Etat(parent=None, liste_deplacement=[],
+                      cout=heuristique(K, plateau_initial))]
+    explored = set()
     # l'état finale à une heuristique de 0 : toutes les cases sont à la bonnes position.
 
-    explored = set()
-
-    min_heuri = 2000
-
-    while True:
-        if frontiere == []:
-            return None
-
-        print(frontiere)
-
+    while len(frontiere) != 0:
         etat_choisi = frontiere.pop(0)
+        plateau = deplacement(etat_choisi.liste_deplacement, plateau_initial)
 
-        if etat_choisi.heuristique == 0:
+        if heuristique(K, plateau) == 0:
             return etat_choisi
             # ici on retroune la solution. Faire une fct qui calcul la solution si besoins.
         else:
             # S est une liste contenant tous les états trouvé après l'expention
             S = expanse(plateau_initial, etat_choisi)
             for etat_cree in S:
-                inserer_etat(frontiere, etat_cree)
+                if tuple(deplacement(etat_cree.liste_deplacement, plateau_initial)) not in explored:
+                    inserer_etat(frontiere, etat_cree)
+
+        explored.add(tuple(plateau))
+    return None
+
 
 def get_poids_tuile(k: int, i: int):
     if i > NOMBRE_TUILES:
@@ -105,35 +103,40 @@ def swap(l, i, j):
 
 def move_line(plateau, ligne, dir: int):
     n = DIM_GRILLE
-    r = range(ligne*n+1, ligne*n + n)
-    if dir > 0:
-        r = reversed(r)
 
-    for i in r:
-        swap(plateau, i-1, i % n)
-
+    if dir == 1:
+        for i in reversed(range(ligne * n, (ligne + 1)*n - 1)):
+            plateau[i+1] = plateau[i]
+        plateau[ligne*n] = -1
+    elif dir == -1:
+        for i in range(ligne * n, (ligne + 1)*n - 1):
+            plateau[i] = plateau[i+1]
+        plateau[(ligne + 1)*n - 1] = -1
+        
 # dir = -1 => Vers le Sud
 # dir = 1 => Vers le Nord
 
 
 def move_colone(plateau, colone, dir: int):
     n = DIM_GRILLE
-    r = range(colone + n, n*n, n)
-    if dir < 0:
-        r = reversed(r)
-
-    for i in r:
-        swap(plateau, i, (i + n) % (n*n))
-
+    
+    if dir == 1:
+        for i in range(colone + n, n*n, n):
+            plateau[i-n] = plateau[i]
+        plateau[n*(n-1) + colone] = -1
+    elif dir == -1:
+        for i in reversed(range(colone + n, n * (n), n)):
+            plateau[i] = plateau[i-n]
+        plateau[colone] = -1
 
 # la fonction permet de déplacer la case vide sur notre taquin.
 # directions est une liste de direction. Une direction peut être N,S,E,O. Ou N=Nord, S=Sud, O=Ouest, E=Est
 # n est la taille du tableau nxn ou 3x3 par exemple
 def deplacement(directions, plateau_initial):
     n = DIM_GRILLE
-    plateau = plateau_initial
+    plateau = plateau_initial[:]
     for dir in directions:
-        pos_case_vide = plateau_initial.index(-1)
+        pos_case_vide = plateau.index(-1)
         if dir == 'N':
             if 0 <= pos_case_vide < n:
                 move_colone(plateau, pos_case_vide, 1)
@@ -146,7 +149,7 @@ def deplacement(directions, plateau_initial):
                 swap(plateau, pos_case_vide, pos_case_vide+n)
         elif dir == 'O':
             if pos_case_vide in [x for x in range(0, n*n, n)]:
-                move_line(plateau, pos_case_vide//n, -1)
+                move_line(plateau, pos_case_vide // n, -1)
                 # [x for x in range(0, n*n, n)] -> permet de créer une liste par palier de n si n =3 on aura: [0,3,6]
             else:
                 swap(plateau, pos_case_vide, pos_case_vide-1)
@@ -161,5 +164,8 @@ def deplacement(directions, plateau_initial):
 
 # main
 if __name__ == '__main__':
-    plateau = [1, 3, 0, 5, 7, 6, 4, 2, -1]
-    print(astar(plateau))
+    K = 0
+    plateau = [1, 3, -1, 5, 7, 6, 4, 2, 0]
+    etat_final = astar(plateau)
+    if etat_final is not None:
+        print(etat_final.liste_deplacement)
